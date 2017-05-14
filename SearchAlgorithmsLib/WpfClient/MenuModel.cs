e00514cb2b6c4acc067eaea47882275bb48765a1
Rina <Rina@WINDOWS-O4DFLS1>
2017-05-14 17:16:21 +0300

    changes

---
+++ SearchAlgorithmsLib/WpfClient/MenuModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClient
+{
+    public class MenuModel : IMenuModel
+    {
+        public int MazeRows
+        {
+            get { return Properties.Settings.Default.MazeRows; }
+            set {; }
+        }
+
+        public int MazeCols
+        {
+            get { return Properties.Settings.Default.MazeCols; }
+            set {; }
+        }
+    }
+}
