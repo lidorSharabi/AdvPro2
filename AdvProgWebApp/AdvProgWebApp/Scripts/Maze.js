7b2dc0ed2768619605f9c9760c8712170837bca9
Rina <Rina@WINDOWS-O4DFLS1>
2017-06-08 16:43:24 +0300

    single

---
+++ AdvProgWebApp/AdvProgWebApp/Scripts/Maze.js
@@ -0,0 +1,16 @@
+﻿var myMazeBoard = $("#mazeCanvas").mazeBoard(
+    mazeData = [[0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], // the matrix containing the maze cells
+    startRow = mazeData.length, startCol = mazeData[0].length, // initial position of the player
+    exitRow = 0, exitCol = 1, // the exit position
+    playerImage = new Image(), // player's icon (of type Image)
+    exitImage = new Image, // exit's icon (of type Image)
+        true, // is the board enabled (i.e., player can move)
+        playerImage.src("Images/dog.jpg")
+    function (direction, playerRow, playerCol) {
+        // a callback function which is invoked after each move
+    }
+}
