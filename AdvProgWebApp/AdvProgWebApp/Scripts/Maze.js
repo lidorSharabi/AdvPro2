73221622474f8f4ee0081e0a0dd871b99fb336b7
Rina <Rina@WINDOWS-O4DFLS1>
2017-06-09 17:45:35 +0300

    commit

--- AdvProgWebApp/AdvProgWebApp/Scripts/Maze.js
+++ AdvProgWebApp/AdvProgWebApp/Scripts/Maze.js
@@ -1,16 +1,20 @@
-﻿var myMazeBoard = $("#mazeCanvas").mazeBoard(
-    mazeData = [[0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-    [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], // the matrix containing the maze cells
-    startRow = mazeData.length, startCol = mazeData[0].length, // initial position of the player
-    exitRow = 0, exitCol = 1, // the exit position
-    playerImage = new Image(), // player's icon (of type Image)
-    exitImage = new Image, // exit's icon (of type Image)
-        true, // is the board enabled (i.e., player can move)
-        playerImage.src("Images/dog.jpg")
-    function (direction, playerRow, playerCol) {
-        // a callback function which is invoked after each move
-    }
-}
+﻿(function ($) {
+    $.fn.mazeBoard = function () {
+        function drawMaze(maze) {
+            var rows = $(this).mazeData.length;
+            var cols = $(this).mazeData[0].length;
+            var cellWidth = $(this).width() / cols;
+            var cellHeight = $(this).height() / rows;
+            for (var i = 0; i < rows; i++) {
+                for (var j = 0; j < cols; j++) {
+                    if (maze[i][j] == 1) {
+                        context.fillRect(cellWidth * j, cellHeight * i,
+                            cellWidth, cellHeight);
+                    }
+                }
+            }
+        }
+        drawMaze(maze);
+        return this;
+    };
+})(jQuery);
