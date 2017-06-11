75dc03fd34f0e6d3e79fd96eff145f61a65e0948
Rina <Rina@WINDOWS-O4DFLS1>
2017-06-11 22:03:10 +0300

    changes

---
+++ AdvProgWebApp/AdvProgWebApp/Model/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Server
+{
+    /// <summary>
+    /// responisble for controling all the commands that comes from the clients
+    /// </summary>
+    public class Controller
+    {
+        /// <summary>
+        /// dictionary of the command and his name, the name is the key
+        /// </summary>
+        private Dictionary<string, ICommand> commands;
+        /// <summary>
+        /// the model of the program
+        /// </summary>
+        private IModel model;
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public Controller()
+        {
+            model = new Model();
+            commands = new Dictionary<string, ICommand>();
+            commands.Add("generate", new GenerateMazeCommand(model));
+            commands.Add("solve", new SolveMazeCommand(model));
+            commands.Add("start", new StartMazeCommand(model));
+            commands.Add("list", new ListMazeCommand(model));
+            commands.Add("join", new JoinMazeCommand(model));
+            commands.Add("play", new PlayMazeCommand(model));
+            commands.Add("close", new CloseMazeCommand(model));
+
+        }
+
+        /// <summary>
+        /// receives the command from the view and passes it to the model
+        /// by executing the matching function for the command
+        /// </summary>
+        /// <param name="commandLine"></param>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public string ExecuteCommand(string commandLine, TcpClient client)
+        {
+            string[] arr = commandLine.Split(' ');
+            string commandKey = arr[0];
+            if (!commands.ContainsKey(commandKey))
+                return "Command not found";
+            string[] args = arr.Skip(1).ToArray();
+            ICommand command = commands[commandKey];
+            return command.Execute(args, client);
+        }
+    }
+}
---
+++ AdvProgWebApp/AdvProgWebApp/Model/HandleMultiplaterGame.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Server
+{
+    class HandleMultiplaterGame
+    {
+        public TcpClient host { get; set; }
+        public TcpClient guest { get; set; }
+        public string gameToJason { get; set; }
+        public string name { get; set; }
+        private Controller control;
+        private bool run = true;
+
+        public HandleMultiplaterGame(TcpClient host, Controller control)
+        {
+            this.host = host;
+            this.control = control;
+        }
+
+        public void startBothClients()
+        {
+            startThread(host);
+            startThread(guest);
+        }
+
+        private void startThread(TcpClient client)
+        {
+            new Task(() =>
+            {
+                string commandLine;
+                Response result;
+                NetworkStream stream = client.GetStream();
+                StreamReader reader = new StreamReader(stream);
+                StreamWriter writer = new StreamWriter(stream);
+                while (run)
+                {
+                    try
+                    {
+                        commandLine = reader.ReadLine();
+                        Console.WriteLine("Got command: {0}", commandLine);
+                        commandLine = commandLine + " " + name;
+                        result = control.ExecuteCommand(commandLine, client);
+                        writer.Write(result);
+                    }
+                    catch
+                    {
+                        return;
+                    }
+                }
+            }).Start();
+        }
+
+        public void sendMazeToJsonToHost()
+        {
+            NetworkStream stream = host.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(gameToJason);
+        }
+
+        public void close()
+        {
+            run = false;
+            host.Close();
+            guest.Close();
+        }
+
+        public void startHost()
+        {
+            startThread(host);
+        }
+
+        public void startGuest()
+        {
+            startThread(guest);
+        }
+
+        public void sendMessageToClient(TcpClient client)
+        {
+            string[] palyMoveFormat = new string[2];
+            palyMoveFormat[0] = "''Name'':''mygame'',";
+            palyMoveFormat[1] = "''Direction'': ''right''";
+            NetworkStream stream;
+            StreamWriter writer;
+            try
+            {
+                if (client == host)
+                {
+                    stream = guest.GetStream();
+                    writer = new StreamWriter(stream);
+                }
+                else if (client == guest)
+                {
+                    stream = host.GetStream();
+                    writer = new StreamWriter(stream);
+                }
+                else return;
+                writer.Write(JsonConvert.SerializeObject(palyMoveFormat));
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+}
