d48c758f9977e8da6299246c187f161d5330dfe2
LIDOR-PC <lidor206@gmail.com>
2017-06-16 15:45:52 +0300

    add account validations

--- AdvProg2WebApp/AdvProg2WebApp/Scripts/Menu.js
+++ AdvProg2WebApp/AdvProg2WebApp/Scripts/Menu.js
@@ -2,19 +2,49 @@
     $("#menu").load("../Menu.html");
 });
 
-var userName;
-
 function addUser() {
     var usersUrl = "api/Users/";
-    userName = $("#signup-username").val();
+    var userName = $("#signup-username").val();
+    var email = $("#signup-email").val();
+    var password = $("#signup-password").val();
+    var confrimPassword = $("#signup-confrim-password").val();
+
     var user = {
-        UserNameId: $("#signup-username").val(),
+        UserNameId: userName,
         Losses: 0,
-        Victories: 0
+        Victories: 0,
+        MailAddress: email,
+        Password: password
     };
 
-    if (userName === "") {
+    var validation = false;
+    if (userName == "") {
         document.getElementById("signup-userName-required").classList.add('is-visible');
+        validation = true;
+    }
+    if (!validateEmail(email)) {
+        document.getElementById("signup-email-required").classList.add('is-visible');
+        document.getElementById("signup-email-required").textContent = "Email format should be john@doe.com.";
+        validation = true;
+    }
+    if (email == "") {
+        document.getElementById("signup-email-required").classList.add('is-visible');
+        document.getElementById("signup-email-required").textContent = "Required field";
+        validation = true;
+    }
+    if (password == "") {
+        document.getElementById("signup-password-required").classList.add('is-visible');
+        validation = true;
+    }
+    if (confrimPassword == "") {
+        document.getElementById("signup-confrim-password-required").classList.add('is-visible');
+        validation = true;
+    }
+    if (password != confrimPassword) {
+        document.getElementById("signup-confrim-password-required").textContent = "Password mismatch";
+        validation = true;
+    }
+    if (validation) {
         return;
     }
 
@@ -22,9 +52,30 @@ function addUser() {
         document.getElementById("cd-user-modal").classList.remove('is-visible');
 
     }).fail(function (response) {
-        if (response.statusText === "Conflict") {
+        if (response.statusText == "Conflict") {
             alert("ERROR: this user already exist \r\n please try again with different name");
         }
         document.getElementById("signup-username").value = "";
     });
+}
+
+function usernameChange() {
+    document.getElementById("signup-userName-required").classList.remove('is-visible');
+}
+
+function emailChange() {
+    document.getElementById("signup-email-required").classList.remove('is-visible');
+}
+
+function passWordChange() {
+    document.getElementById("signup-password-required").classList.remove('is-visible');
+}
+
+function confrimPassWordChange() {
+    document.getElementById("signup-confrim-password-required").classList.remove('is-visible');
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
