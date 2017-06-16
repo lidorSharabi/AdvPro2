4a5f40fcc99129990359b8b4837a8481a4033aeb
LIDOR-PC <lidor206@gmail.com>
2017-06-16 14:05:54 +0300

    create new user validation (user name empty ot exist)

--- AdvProg2WebApp/AdvProg2WebApp/Scripts/Menu.js
+++ AdvProg2WebApp/AdvProg2WebApp/Scripts/Menu.js
@@ -2,14 +2,29 @@
     $("#menu").load("../Menu.html");
 });
 
+var userName;
+
 function addUser() {
-        var usersUrl = "api/Users/";
-        var user = {
-            UserNameId: $("#signup-username").val(),
-            Losses: 0,
-            Victories: 0
-        };
-        $.post(usersUrl, user).done(function (data) {
-            //self.books.push(data);
-        });
+    var usersUrl = "api/Users/";
+    userName = $("#signup-username").val();
+    var user = {
+        UserNameId: $("#signup-username").val(),
+        Losses: 0,
+        Victories: 0
+    };
+
+    if (userName === "") {
+        document.getElementById("signup-userName-required").classList.add('is-visible');
+        return;
+    }
+
+    $.post(usersUrl, user).done(function (data) {
+        document.getElementById("cd-user-modal").classList.remove('is-visible');
+
+    }).fail(function (response) {
+        if (response.statusText === "Conflict") {
+            alert("ERROR: this user already exist \r\n please try again with different name");
+        }
+        document.getElementById("signup-username").value = "";
+    });
 }
